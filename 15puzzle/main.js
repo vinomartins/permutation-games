50e930eb693399176dd7635fc56e41d7e4e68ce9
Vinicius Martins <martins.vinicius@gmail.com>
2022-06-29 19:08:09 -0300

    Retorna com as def. de row-end e column-end

--- 15puzzle/main.js
+++ 15puzzle/main.js
@@ -28,10 +28,10 @@ $(document).ready(function () {
   $(".cards-wrapper").hover(
     function () {
       let coord = getCoord($(this));
-      
+      console.log($(this).attr("id"));
       if (allowedMove(coord.col, coord.row)) {
         $(this).addClass("available-card");
-      } else {
+      } else if ($(this).attr("id") != "card-00") {
         $(this).addClass("unavailable-card");
       }
     },
@@ -39,16 +39,19 @@ $(document).ready(function () {
       $(this).removeClass("available-card unavailable-card");
     }
   );
+  
+  $(".divlink").click(function (){
+    // TODO: Inicia novo jogo com posição aleatória
+    console.log("Inicia novo jogo com posição aleatória");
+  })
 
   $(".cards-wrapper").click(newFunctionCLICOU());
-  //$(".cards-wrpper").makeMove
 });
-// teste 3
+
+
 function newFunctionCLICOU() {
   return function () {
     // define o estado inicial
-    defineInitialState();
-
     if (allowedMove(colClickJS, rowClickJS)) {
       makeMove(colClickJS, rowClickJS, $(this));
     } else console.log("Impossível mover essa peça");
@@ -64,34 +67,44 @@ function makeMove(colClickJS, rowClickJS, card) {
     // Assim, o índice final (no CSS) vai ser
     // (rowClickJS - 1) + 1 = rowClickJS
     card.css("grid-row-start", rowClickJS);
+    card.css("grid-row-end", rowClickJS);
     // Movendo o emptyCard para a posição nova
     emptyCoord = [emptyCoord[0], emptyCoord[1] + 1];
     $("#card-00").css("grid-row-start", rowClickJS + 1);
+    $("#card-00").css("grid-row-end", rowClickJS + 1);
     j = i - 4;
   } else if (above(colClickJS, rowClickJS)) {
     // Movendo a peça para a posição nova
     card.css("grid-row-start", rowClickJS + 2);
+    card.css("grid-row-end", rowClickJS + 2);
 
     // Movendo o emptyCard para a posição nova
     emptyCoord = [emptyCoord[0], emptyCoord[1] - 1];
     $("#card-00").css("grid-row-start", rowClickJS + 1);
+    $("#card-00").css("grid-row-end", rowClickJS + 1);
+
     j = i + 4;
   } else if (left(colClickJS, rowClickJS)) {
     // Movendo a peça para a posição nova
     card.css("grid-column-start", colClickJS + 2);
+    card.css("grid-column-end", colClickJS + 2);
 
     // Movendo o emptyCard para a posição nova
     emptyCoord = [emptyCoord[0] - 1, emptyCoord[1]];
     $("#card-00").css("grid-column-start", colClickJS + 1);
+    $("#card-00").css("grid-column-end", colClickJS + 1);
+
     j = i + 1;
   } else {
     // right(x,y)
     // Movendo a peça para a posição nova
     card.css("grid-column-start", colClickJS);
+    card.css("grid-column-end", colClickJS);
 
     // Movendo o emptyCard para a posição nova
     emptyCoord = [emptyCoord[0] + 1, emptyCoord[1]];
     $("#card-00").css("grid-column-start", colClickJS + 1);
+    $("#card-00").css("grid-column-end", colClickJS + 1);
     j = i - 1;
   }
 
@@ -99,23 +112,19 @@ function makeMove(colClickJS, rowClickJS, card) {
 }
 
 function changeGameStatus(i, j) {
-  // console.log("i = " + i + "j = " + j);
-  // console.log(inicial);
+  // verifica se o puzzle foi resolvido
+  // (essa funçao devia ter outro nome)
   inicial[j] = inicial[i];
   inicial[i] = 0;
-  // console.log(inicial);
   for (let index = 0; index < 15; index++) {
     if (inicial[index] != index + 1) {
-      // console.log('posição do 00: ' + index);
       return;
     }
   }
-
   endOfGame();
 }
 
 function allowedMove(x, y) {
-  // console.log("card vazio:" + emptyCoord)
   return under(x, y) || above(x, y) || left(x, y) || right(x, y);
 }
 
